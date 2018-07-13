4b99712b266c6b2d810e991488c96708b5d6d9ee
Ksenia <ksenya-f3@rambler.ru>
2018-07-13 21:37:41 +0300

    The First commit

---
+++ SortingAlgorithms/SortJaggedArray.cs
@@ -0,0 +1,193 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    /// <summary>
+    /// Class for sorting jagged arrays
+    /// </summary>
+    public static class SortJaggedArray
+    {
+        /// <summary>
+        /// BubbleSort for jagged arrays.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <param name="ascendingOrder">if set to <c>true</c> [ascending order].</param>
+        /// <param name="sortingBy">The method of sorting.</param>
+        /// <returns>
+        /// Sorted array.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Jagged array shouldn't be null. - array
+        /// or
+        /// Elements of jagged array shouldn't be null. - array
+        /// or
+        /// Elements of jagged array shouldn't be empty. - array
+        /// </exception>
+        /// <exception cref="ArgumentException">There is no such sorting of jagged array - sortingBy</exception>
+        public static int[][] BubbleSort(this int[][] array, bool ascendingOrder, string sortingBy)
+        {
+            array.BubbleSort2(ascendingOrder, sortingBy);
+            return array;
+        }
+
+        /// <summary>
+        /// BubbleSort for jagged arrays
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <param name="ascendingOrder">if set to <c>true</c> [ascending order].</param>
+        /// <param name="sortingBy">The method of sorting.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Jagged array shouldn't be null. - array
+        /// or
+        /// Elements of jagged array shouldn't be null. - array
+        /// or
+        /// Elements of jagged array shouldn't be empty. - array
+        /// </exception>
+        /// <exception cref="ArgumentException">There is no such sorting of jagged array - sortingBy</exception>
+        public static void BubbleSort2(this int[][] array, bool ascendingOrder, string sortingBy)
+        {
+            if (array == null) 
+            {
+                throw new ArgumentNullException("Jagged array shouldn't be null.", nameof(array));
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == null)
+                {
+                    throw new ArgumentNullException("Elements of jagged array shouldn't be null.", nameof(array));
+                }
+
+                if (array[i].Length == 0)
+                {
+                    throw new ArgumentNullException("Elements of jagged array shouldn't be empty.", nameof(array));
+                }
+            }
+
+            if (array.Length == 1)
+            {
+                return;
+            }
+
+            switch (sortingBy)
+            {
+                case "Sum":
+                    {
+                        SortBySum(array, ascendingOrder);
+                        break;
+                    }
+
+                case "Max Element":
+                    {
+                        SortByMaxEl(array, ascendingOrder);
+                        break;
+                    }
+
+                case "Min Element":
+                    {
+                        SortByMinEl(array, ascendingOrder);
+                        break;
+                    }
+
+                default:
+                    {
+                        throw new ArgumentException("There is no such sorting of jagged array", nameof(sortingBy));
+                    }                   
+            }
+        }
+
+        private static void SortByMinEl(int[][] jaggedArray, bool ascendingOrder)
+        {
+            int[] minsArray = new int[jaggedArray.Length];
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                minsArray[i] = jaggedArray[i][0];
+                for (int j = 1; j < jaggedArray[i].Length; j++)
+                {
+                    if (jaggedArray[i][j] < minsArray[i])
+                    {
+                        minsArray[i] = jaggedArray[i][j];
+                    }
+                }
+            }
+
+            BubbleSort(minsArray, jaggedArray, ascendingOrder);
+        }
+
+        private static void SortByMaxEl(int[][] jaggedArray, bool ascendingOrder)
+        {
+            int[] maxsArray = new int[jaggedArray.Length];
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                maxsArray[i] = jaggedArray[i][0];
+                for (int j = 1; j < jaggedArray[i].Length; j++)
+                {
+                    if (jaggedArray[i][j] > maxsArray[i]) 
+                    {
+                        maxsArray[i] = jaggedArray[i][j];
+                    }
+                }
+            }
+
+            BubbleSort(maxsArray, jaggedArray, ascendingOrder);
+        }
+
+        private static void SortBySum(int[][] array, bool ascendingOrder)
+        {
+            int[] sumsArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    sumsArray[i] += array[i][j];
+                }
+            }
+
+            BubbleSort(sumsArray, array, ascendingOrder);
+        }
+
+        private static void BubbleSort(int[] array, int[][] jaggedArray, bool ascendingOrder)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+
+            if (!ascendingOrder) 
+            {
+                Reverse(jaggedArray);
+            }
+        }
+
+        private static void Reverse(int[][] jaggedArray)
+        {
+            for (int i = 0; i < jaggedArray.Length / 2; i++)
+            {
+                Swap(ref jaggedArray[i], ref jaggedArray[jaggedArray.Length - 1 - i]);
+            }
+        }
+
+        /// <summary>
+        /// Swaps two referenses of the arays in jagged array
+        /// </summary>
+        /// <param name="el1">
+        /// The first reference
+        /// </param>
+        /// <param name="el2">
+        /// The second reference 
+        /// </param>
+        private static void Swap<T>(ref T el1, ref T el2)
+        {
+            T temp = el1;
+            el1 = el2;
+            el2 = temp;
+        }
+    }
+}
---
+++ SortingAlgorithmsTests/SortJaggedArrayTests.cs
@@ -0,0 +1,179 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SortingAlgorithms.Tests
+{
+    [TestFixture()]
+    public class SortJaggedArrayTests
+    {
+        private readonly int[][][] array =
+            {
+                new int[][]{
+                    new int[]{ 1, 2, 8, 9 },
+                    new int[]{ 2, 21, 0 },
+                    new int[]{ 5 },
+                    new int[]{ 20, 10 }},
+                new int[][]{
+                    new int[]{ -5, 1, 892, 78, 9, 0 }, //975
+                    new int[]{ 2, 21, 0, 89, -89, 78 }, //101
+                    new int[]{ 5, 9, -8, 38, 24, -17 },//51
+                    new int[]{ 61, 35, 94, 81, 33, 17, 20, 10 },//351
+                    new int[]{ 22, 6, 48, -1, 8},//83
+                    new int[]{ 0 }
+            }
+        };
+
+        private readonly int[][][] resultSumAsc =
+        {
+             new int[][]{
+                 new int[]{ 5 },
+                new int[]{ 1, 2, 8, 9 },
+                new int[]{ 2, 21, 0 },
+                new int[]{ 20, 10 }},
+             new int[][]{
+                    new int[]{ 0 },
+                    new int[]{ 5, 9, -8, 38, 24, -17 },//51
+                    new int[]{ 22, 6, 48, -1, 8},//83
+                    new int[]{ 2, 21, 0, 89, -89, 78 }, //101
+                    new int[]{ 61, 35, 94, 81, 33, 17, 20, 10 },//351
+                    new int[]{ -5, 1, 892, 78, 9, 0 } //975
+             }
+        };
+
+        private readonly int[][][] resultSumDesc =
+        {
+            new int[][]{
+                new int[]{ 20, 10 },
+                new int[]{ 2, 21, 0 },
+                new int[]{ 1, 2, 8, 9 },
+                new int[]{ 5 }},
+            new int[][]{
+                    new int[]{ -5, 1, 892, 78, 9, 0 }, //975
+                    new int[]{ 61, 35, 94, 81, 33, 17, 20, 10 },//351
+                    new int[]{ 2, 21, 0, 89, -89, 78 }, //101
+                    new int[]{ 22, 6, 48, -1, 8},//83
+                    new int[]{ 5, 9, -8, 38, 24, -17 },//51
+                    new int[]{ 0 }
+            }
+            };
+
+        private readonly int[][][] resultMaxAsc =
+        {
+             new int[][]{
+                new int[]{ 5 },
+                new int[]{ 1, 2, 8, 9 },
+                new int[]{ 20, 10 },
+                new int[]{ 2, 21, 0 } },
+              new int[][]{
+                    new int[]{ 0 },
+                    new int[]{ 5, 9, -8, 38, 24, -17 },
+                    new int[]{ 22, 6, 48, -1, 8},
+                    new int[]{ 2, 21, 0, 89, -89, 78 },
+                    new int[]{ 61, 35, 94, 81, 33, 17, 20, 10 },
+                    new int[]{ -5, 1, 892, 78, 9, 0 }
+              }
+            };
+
+        private readonly int[][][] resultMaxDesc =
+        {
+              new int[][]{
+                new int[]{ 2, 21, 0 },
+                new int[]{ 20, 10 },
+                new int[]{ 1, 2, 8, 9 },
+                new int[]{ 5 } },
+              new int[][]{
+                    new int[]{ -5, 1, 892, 78, 9, 0 },
+                    new int[]{ 61, 35, 94, 81, 33, 17, 20, 10 },
+                    new int[]{ 2, 21, 0, 89, -89, 78 },
+                    new int[]{ 22, 6, 48, -1, 8},
+                    new int[]{ 5, 9, -8, 38, 24, -17 },
+                    new int[]{ 0 }
+              }
+            };
+
+        private readonly int[][][] resultMinAsc =
+        {
+              new int[][]{
+                 new int[]{ 2, 21, 0 },
+                 new int[]{ 1, 2, 8, 9 },
+                 new int[]{ 5 },
+                 new int[]{ 20, 10 } },
+              new int[][]{
+                    new int[]{ 2, 21, 0, 89, -89, 78 },
+                    new int[]{ 5, 9, -8, 38, 24, -17 },
+                    new int[]{ -5, 1, 892, 78, 9, 0 },
+                    new int[]{ 22, 6, 48, -1, 8},
+                    new int[]{ 0 },                    
+                    new int[]{ 61, 35, 94, 81, 33, 17, 20, 10 }
+              }
+            };
+
+        private readonly int[][][] resultMinDesc =
+        {
+              new int[][]{
+                new int[]{ 20, 10 },
+                new int[]{ 5 },
+                new int[]{ 1, 2, 8, 9 },
+                new int[]{ 2, 21, 0 } },
+              new int[][]{
+                  new int[]{ 61, 35, 94, 81, 33, 17, 20, 10 },
+                  new int[]{ 0 },
+                  new int[]{ 22, 6, 48, -1, 8},
+                  new int[]{ -5, 1, 892, 78, 9, 0 },
+                  new int[]{ 5, 9, -8, 38, 24, -17 },
+                  new int[]{ 2, 21, 0, 89, -89, 78 },
+              } 
+            };
+
+        private int[][] nullArray = null;
+        private int[][] emptyArray = { };
+
+        [Test]
+        public void BubbleSortTest_SumAsc1() => CollectionAssert.AreEqual(array[0].BubbleSort(true, "Sum"), resultSumAsc[0]);
+
+        [Test]
+        public void BubbleSortTest_SumAsc2() => CollectionAssert.AreEqual(array[1].BubbleSort(true, "Sum"), resultSumAsc[1]);
+
+        [Test]
+        public void BubbleSortTest_SumDesc1() => CollectionAssert.AreEqual(array[0].BubbleSort(false, "Sum"), resultSumDesc[0]);
+
+        [Test]
+        public void BubbleSortTest_SumDesc2() => CollectionAssert.AreEqual(array[1].BubbleSort(false, "Sum"), resultSumDesc[1]);
+
+        [Test]
+        public void BubbleSortTest_MaxAsc1() => CollectionAssert.AreEqual(array[0].BubbleSort(true, "Max Element"), resultMaxAsc[0]);
+
+        [Test]
+        public void BubbleSortTest_MaxAsc2() => CollectionAssert.AreEqual(array[1].BubbleSort(true, "Max Element"), resultMaxAsc[1]);
+
+        [Test]
+        public void BubbleSortTest_MaxDesc1() => CollectionAssert.AreEqual(array[0].BubbleSort(false, "Max Element"), resultMaxDesc[0]);
+
+        [Test]
+        public void BubbleSortTest_MaxDesc2() => CollectionAssert.AreEqual(array[1].BubbleSort(false, "Max Element"), resultMaxDesc[1]);
+
+        [Test]
+        public void BubbleSortTest_MinAsc1() => CollectionAssert.AreEqual(array[0].BubbleSort(true, "Min Element"), resultMinAsc[0]);
+
+        [Test]
+        public void BubbleSortTest_MinAsc2() => CollectionAssert.AreEqual(array[1].BubbleSort(true, "Min Element"), resultMinAsc[1]);
+
+        [Test]
+        public void BubbleSortTest_MinDesc1() => CollectionAssert.AreEqual(array[0].BubbleSort(false, "Min Element"), resultMinDesc[0]);
+
+        [Test]
+        public void BubbleSortTest_MinDesc2() => CollectionAssert.AreEqual(array[1].BubbleSort(false, "Min Element"), resultMinDesc[1]);
+
+        [Test]
+        public void BubbleSortTest_InValidMetod()
+            => Assert.Throws<ArgumentException>(() => array[0].BubbleSort(true, "Mult"));
+
+        [Test]
+        public void BubbleSortTest_nullarray()
+            => Assert.Throws<ArgumentNullException>(() => nullArray.BubbleSort(true, "Sum"));
+
+        [Test]
+        public void BubbleSortTest_emptyarray()
+        => Assert.Throws<ArgumentException>(() => emptyArray.BubbleSort(true, "Mult"));
+    }
+}
