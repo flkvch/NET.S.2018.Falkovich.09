34a8483bef8e091cf6c0128ad2402d0f1bcc7ace
Ksenia <ksenya-f3@rambler.ru>
2018-07-15 01:47:00 +0300

    The second task added

---
+++ Bank/BankAccount.cs
@@ -0,0 +1,188 @@
+﻿using System;
+
+namespace Bank
+{
+    /// <summary>
+    /// Privelege of BankAccount
+    /// </summary>
+    public enum Privilege
+    {
+        Base = 2,
+        Silver = 4,
+        Gold = 6,
+        Platinum = 8
+    }
+
+    /// <summary>
+    /// Client Information
+    /// </summary>
+     public struct Client
+    {
+        private string firstName;
+        private string lastName;
+        private string dateOfBirth;
+        private string passportData;
+        private string telephone;
+        private string adress;   
+        
+        public Client(string firstName, string lastName, string dateOfBirth, string passportData, string telephone, string adress)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.dateOfBirth = dateOfBirth;
+            this.adress = adress;
+            this.passportData = passportData;
+            this.telephone = telephone;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Client: \n{firstName} \n{ lastName} \n{ dateOfBirth} \n{ adress}" +
+            $"\n{ passportData}\n{ telephone}");
+        }
+    }
+
+    /// <summary>
+    /// Bank Account
+    /// </summary>
+    public class BankAccount
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BankAccount"/> class.
+        /// </summary>
+        /// <param name="client">The client.</param>
+        /// <param name="priority">The priority.</param>
+        public BankAccount(Client client, int priority)
+        {
+            ++NumberOfAccount;
+            this.Client = client;
+            Balance = 0;
+            Privilege = (Privilege)priority;
+            IsActive = true;
+            //Console.WriteLine($"Account #{NumberOfAccount} is created");
+        }
+
+        /// <summary>
+        /// Gets or sets the balance.
+        /// </summary>
+        /// <value>
+        /// The balance.
+        /// </value>
+        public int Balance { get; private set; }
+
+        public int NumberOfAccount { get; private set; } = 0;
+
+        private Client Client { get; set; }
+
+        private Privilege Privilege { get; set; }
+
+        /// <summary>
+        /// Gets or sets the bonus points.
+        /// </summary>
+        /// <value>
+        /// The bonus points.
+        /// </value>
+        public int BonusPoints { get; private set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is active.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is active; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsActive { get; private set; }
+
+        /// <summary>
+        /// Refills the specified sum.
+        /// </summary>
+        /// <param name="sum">The sum.</param>
+        /// <exception cref="System.InvalidOperationException">The account isn't active.</exception>
+        public void Refill(int sum)
+        {
+            if (!IsActive)
+            {
+                throw new InvalidOperationException("The account isn't active.");
+            }
+
+            BonusPoints += CountRefillBonus(sum);   
+            Balance += sum;
+        }
+
+        /// <summary>
+        /// Withdraws the specified sum.
+        /// </summary>
+        /// <param name="sum">The sum.</param>
+        /// <exception cref="System.InvalidOperationException">The account isn't active.</exception>
+        /// <exception cref="System.ArgumentException"></exception>
+        public void Withdraw(int sum)
+        {
+            if (!IsActive)
+            {
+                throw new InvalidOperationException("The account isn't active.");
+            }
+
+            if (sum > Balance)
+            {
+                throw new ArgumentException($"You have only {Balance} on your " +
+                    $"Balance and you've tried to withdraw {sum}.");
+            }
+
+            BonusPoints -= CountWithdrawBonus(sum);
+            Balance -= sum;
+        }
+
+        /// <summary>
+        /// Deletes the account.
+        /// </summary>
+        /// <exception cref="System.InvalidOperationException">The account is already deleted.</exception>
+        public void DeleteAccount()
+        {
+            if (!IsActive)
+            {
+                throw new InvalidOperationException("The account is already deleted.");
+            }
+
+            IsActive = false;
+        }
+
+        /// <summary>
+        /// Activates the account.
+        /// </summary>
+        /// <exception cref="System.InvalidOperationException">Account is already active.</exception>
+        public void ActivateAccount()
+        {
+            if (IsActive)
+            {
+                throw new InvalidOperationException("Account is already active. ");
+            }
+
+            IsActive = true;
+        }
+
+        public void GetStatus()
+        {
+            Console.WriteLine($"The number of account: {NumberOfAccount }");
+            if (IsActive)
+            {
+                Console.WriteLine("Is active");
+            }
+            else
+            {
+                Console.WriteLine("is not active");
+            }
+
+            Console.WriteLine($"Balance: {Balance}");
+            Console.WriteLine($"Privelege: {Privilege}");
+            Console.WriteLine($"Bonus points: { BonusPoints}");
+            Client.Print();
+        }
+
+        private int CostBalance() => (int)Math.Log(Balance + 1, (int)Privilege);
+
+        private int CostRefill(int sum) => (int)Math.Log(sum + 1, (int)Privilege);
+
+        private int CountRefillBonus(int sum) => CostRefill(sum) / (CostBalance() + 1);
+
+        private int CountWithdrawBonus(int sum) => 10 * CostRefill(sum) / (CostBalance() + 1);
+    }
+}
---
+++ ConsoleBankAccount/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank;
+
+namespace ConsoleBankAccount
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount[] bankSystem = new BankAccount[100];
+            int numOfClients = 0;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to the Bank System. " +
+                "\nIf you want to create a new account press -c." +
+                "\nIf you want to exit system press -e.");
+                string input = Console.ReadLine();
+                if (input == "-c")
+                    {
+                        Console.WriteLine("First, you should enter the information about client. \nEnter the firstname:");
+                        string firstName = Console.ReadLine();
+                        Console.WriteLine("\nEnter the lastname:");
+                        string lastName = Console.ReadLine();
+                        Console.WriteLine("\nEnter the day of birth:");
+                        string dateOfBirth = Console.ReadLine();
+                        Console.WriteLine("\nEnter the adress:");
+                        string adress = Console.ReadLine();
+                        Console.WriteLine("\nEnter the passport data:");
+                        string passportData = Console.ReadLine();
+                        Console.WriteLine("\nEnter the telephone:");
+                        string telephone = Console.ReadLine();
+                    Client c1 = new Client(firstName, lastName, dateOfBirth, passportData, telephone, adress);
+                       
+                        Console.WriteLine("Second, you should enter the priority of the bank account:");
+                        int s1;
+                        switch (Console.ReadLine())
+                        {
+                            case "Base":
+                                {
+                                    s1 = 2;
+                                    break;
+                                }
+
+                            case "Silver":
+                                {
+                                    s1 = 4;
+                                    break;
+                                }
+
+                            case "Gold":
+                                {
+                                    s1 = 6;
+                                    break;
+                                }
+
+                            case "Platinum":
+                                {
+                                    s1 = 8;
+                                    break;
+                                }
+
+                            default:
+                                {
+                                    s1 = 2;
+                                    break;
+                                }
+                        }
+
+                    bankSystem[++numOfClients] = new BankAccount(c1, s1);
+
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"You are in the menu of choosing accounts. Enter the number of acc from 1 to {numOfClients}." +
+                            $"\nIf you want to exit enter -e");
+                        string s11 = Console.ReadLine();
+                        if (s11 == "-e")
+                        {
+                            break;
+                        }
+
+                        int num = int.Parse(s11);
+
+                        while (true)
+                        {
+                            BankAccount a1 = bankSystem[num];
+                            Console.Clear();
+                            Console.WriteLine($"You are in the menu of the account number {a1.NumberOfAccount}." +
+                                "\n\nIf yiu want to get status of acc press -s" +
+                                "\n If you want to Refill press -r" +
+                                "\nIf you want to Withdraw press -w" +
+                                "\nIf you want to Delete Account press -d" +
+                                "\nIf you want to Activate deleted Account press -a" +
+                                "\nIf you want to exit this menu press -e");
+
+                            string input2 = Console.ReadLine();
+                            if (input2 == "-s")
+                            {
+                                a1.GetStatus();
+                                Console.ReadLine();
+                            }
+
+                            if (input2 == "-r")
+                            {
+                                Console.Write("Enter the sum of refill: ");
+                                try
+                                {
+                                    a1.Refill(int.Parse(Console.ReadLine()));
+                                }
+                                catch (InvalidOperationException ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+
+                                Console.ReadLine();
+                            }
+
+                            if (input2 == "-w")
+                            {
+                                Console.Write("Enter the sum of withdraw: ");
+                                try
+                                {
+                                    a1.Withdraw(int.Parse(Console.ReadLine()));
+                                }
+                                catch (ArgumentException ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+                                catch (InvalidOperationException ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+
+                                Console.ReadLine();
+                            }
+
+                            if (input2 == "-d")
+                            {
+                                try
+                                {
+                                    a1.DeleteAccount();
+                                }
+                                catch (InvalidOperationException ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+
+                                Console.ReadLine();
+                            }
+
+                            if (input2 == "-a")
+                            {
+                                try
+                                {
+                                    a1.ActivateAccount();
+                                }
+                                catch (InvalidOperationException ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+
+                                Console.ReadLine();
+                            }
+
+                            if (input2 == "-e")
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (input == "-e")
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
